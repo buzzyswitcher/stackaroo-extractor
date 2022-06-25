b85394317358756d8561b17a472486aedc17cc96
evgenii volkov <evgenii.volkov@aisa.ru>
2022-06-25 12:00:47 +0300

    init commit

---
+++ src/test/java/org/buzzyswitcher/stackarooextractor/StackarooExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.buzzyswitcher.stackarooextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackarooExtractorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
