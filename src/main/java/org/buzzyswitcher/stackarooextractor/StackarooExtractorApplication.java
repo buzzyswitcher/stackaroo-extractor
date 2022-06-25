3b6dde8a40b8132a984ab1a3264bcb5341db6e90
evgenii volkov <evgenii.volkov@aisa.ru>
2022-06-25 12:00:42 +0300

    init commit

---
+++ src/main/java/org/buzzyswitcher/stackarooextractor/StackarooExtractorApplication.java
@@ -0,0 +1,13 @@
+package org.buzzyswitcher.stackarooextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackarooExtractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackarooExtractorApplication.class, args);
+    }
+
+}
